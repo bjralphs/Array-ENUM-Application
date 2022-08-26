8248bc1051a741f1fdceb7c345f8041b82084aac
Bryan Ralphs <38300718+bjralphs@users.noreply.github.com>
2022-08-26 12:48:23 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿/*
+I declare that the following source code was written solely by me. 
+I understand that copying any source code, in whole or in part, constitutes cheating, 
+and that I will receive a zero on this project if I am found in violation of this policy.
+*/
+
+using System;
+
+public class LabOne
+{
+    enum Color
+    {
+        black,    // 0
+        brown,   // 1
+        red,      // 2
+        orange,      // 3
+        yellow,        // 4
+        green,       // 5
+        blue,        // 6
+        violet,        // 7
+        grey,        // 8
+        white,        // 9
+    }
+    public static void Main(string[] args)
+    {
+        string colorVar;
+
+        Console.Write("Please input a color... ");
+        //read input
+        colorVar = Console.ReadLine();
+
+        //initialize array method
+        string[] colors = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (colorVar == colors[i])
+            {
+                Console.Write("Array Resistor code: " + colorVar + " ");
+                Console.WriteLine(i.ToString());
+            }
+        }
+        
+        //initialize ENUM method.
+        if (colorVar == "black")
+        {
+            Console.Write("Enum Resistor code: " + colorVar + " ");
+            int myNum = (int)Color.black;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "brown")
+        {
+            Console.Write("Enum Resistor code: " + colorVar + " ");
+            int myNum = (int)Color.brown;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "red")
+        {
+            Console.Write("Enum Resistor code: " + colorVar + " ");
+            int myNum = (int)Color.red;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "orange")
+        {
+            Console.Write("Enum Resistor code: " + colorVar + " ");
+            int myNum = (int)Color.orange;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "yellow")
+        {
+            Console.Write("Enum Resistor code: " + colorVar + " ");
+            int myNum = (int)Color.yellow;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "green")
+        {
+            Console.Write("Enum Resistor code: " + colorVar + " ");
+            int myNum = (int)Color.green;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "blue")
+        {
+            Console.Write("Enum Resistor code: " + colorVar + " ");
+            int myNum = (int)Color.blue;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "violet")
+        {
+            Console.Write("Enum Resistor code: " + colorVar);
+            int myNum = (int)Color.violet;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "grey")
+        {
+            Console.Write("Enum Resistor code: " + colorVar);
+            int myNum = (int)Color.grey;
+            Console.WriteLine(myNum);
+        }
+        if (colorVar == "white")
+        {
+            Console.Write("Enum Resistor code: " + colorVar);
+            int myNum = (int)Color.white;
+            Console.WriteLine(myNum);
+        }
+    }
+}
